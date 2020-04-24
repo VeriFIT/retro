8c5285b1541ffe68fc7961f7df828c2ad3b18d32
vhavlena <vohavlena@gmail.com>
2020-04-24 15:38:37 +0200

    src: vata support #2

--- src/model/RMCLoop.py
+++ src/model/RMCLoop.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import automata.RRTransducer
+import automata.pyvata
 from automata.Symbol import *
 from automata.NFAOperation import *
 from model.ModelCons import *
@@ -62,3 +63,64 @@ class RMCLoop:
             all_nfa = minimalBrzozowski(all_nfa)
             #all_nfa.renameStates()
             nfa_eq = copy(curr_nfa)
+
+
+    @staticmethod
+    def rmc_loop_vata(nfa_eq, rrts):
+        nfa_eq = RMCLoop.fado_to_vata(nfa_eq.toNFA())
+
+        all_nfa = copy(nfa_eq)
+
+        #all_nfa = all_nfa.toDFA()
+        trans_history = list()
+
+        while True:
+            prods = [item.product_vata(nfa_eq) for item in rrts]
+            flatten = list()
+            for item in prods:
+                flatten.append(item.flatten())
+
+            curr_nfa = automata.pyvata.NFA()
+            trans = list()
+            for rrt in flatten:
+                rrt.rename_states()
+                trans.append(rrt)
+
+                vata_aut = rrt.get_nfa_vata()
+
+                vata_aut = vata_aut.removeEpsilon(Epsilon)
+                vata_aut = vata_aut.minimize()
+                #vata_aut.renameStates()
+
+                curr_nfa = automata.pyvata.NFA.union(curr_nfa, vata_aut)
+
+            trans_history.append(trans)
+
+            if curr_nfa.acceptsEpsilon():
+                word = []
+                word = trans_history[-1][1].prod_out_str(word)
+                model_con = ModelCons(trans_history[0:-1])
+                model = model_con.get_model(word)
+                return True, model
+
+            if automata.pyvata.NFA.isIncl(curr_nfa, all_nfa):
+                return False, None
+
+            all_nfa = automata.pyvata.NFA.union(all_nfa, curr_nfa)
+            #all_nfa = toDFA(all_nfa)
+            all_nfa = all_nfa.minimize()
+            nfa_eq = copy(curr_nfa)
+
+
+    @staticmethod
+    def fado_to_vata(nfa):
+        vata = automata.pyvata.NFA()
+        for src, sym_dict in nfa.delta.items():
+            for sym, dst_set in sym_dict.items():
+                for dst in dst_set:
+                    vata.addTransition(src, sym, dst)
+        for i in nfa.Initial:
+            vata.addInitial(i)
+        for i in nfa.Final:
+            vata.addFinal(i)
+        return vata
